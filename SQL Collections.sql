9e3c0329fc9713ce5d8bfab3363a8f5233696beb
rabbit <marialberta98@live.it>
2023-05-14 18:34:32 +0200

    aggiunte relazioni disco-genere e disco-autore

--- SQL Collections.sql
+++ SQL Collections.sql
@@ -31,19 +31,7 @@ create table condivisa(
 	constraint collezionista_collezione foreign key (ID_collezione)
 		references collezione(ID) on delete restrict on update cascade -- da rivedere
     );
-    
-drop table if exists disco;
-create table disco(
-	id_disco integer unsigned auto_increment primary key,
-    titolo varchar(180) not null,
-    barcode integer unsigned,
-    anno smallint unsigned not null,
-    genere integer unsigned,
-    constraint controllo_anno check (anno>1900 and anno<2500),
-    constraint genere_disco foreign key (genere)
-		references genere(id_genere) on delete cascade on update cascade -- da rivedere
-);
-    
+
 drop table if exists genere;
 create table genere(
 	id_genere integer unsigned auto_increment primary key,
@@ -62,7 +50,43 @@ create table traccia(
 	id_genere integer unsigned auto_increment primary key,
     titolo varchar(180) not null,
     durata integer unsigned not null
-); 
+);
+    
+drop table if exists disco;
+create table disco(
+	id_disco integer unsigned auto_increment primary key,
+    titolo varchar(180) not null,
+    barcode integer unsigned,
+    anno smallint unsigned not null,
+    genere_id integer unsigned,
+    autore_id integer unsigned,
+    constraint controllo_anno check (anno>1900 and anno<2500),
+	foreign key (genere_id)
+		references genere(id_genere) on delete cascade on update cascade, -- da rivedere
+	foreign key (autore_id)
+		references autore(id_autore) 
+);
+
+drop table if exists disco_genere;
+create table disco_genere(
+	id_disco integer unsigned, 
+    id_genere integer unsigned,
+    foreign key (id_disco)
+		references disco(id_disco) on delete cascade on update cascade,
+    foreign key (id_genere)
+		references genere(id_genere) on delete cascade on update cascade
+);
+
+drop table if exists disco_autore;
+create table disco_autore(
+	id_disco integer unsigned, 
+    id_autore integer unsigned,
+    foreign key (id_disco)
+		references disco(id_disco) on delete cascade on update cascade,
+    foreign key (id_autore)
+		references autore(id_autore) on delete cascade on update cascade
+);
+
     
     
 
